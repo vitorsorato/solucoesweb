0ce31daeae7c320e0c35eca768b8c605323f5b0f
Robson Alessio <69646857+RobsonAlessio@users.noreply.github.com>
2021-06-29 18:17:59 -0300

    update

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 Auth::routes();
 
 
-Route::get('/home', [HomeController::class, 'index'])->name('orcamento.index-client');
+Route::get('/home', [HomeController::class, 'index-client'])->name('orcamento.index-client');
 
 Route::get('/orcamentos', [OrcamentoController::class, 'index'])->name('orcamento.index');
 Route::get('/orcamentos/criar', [OrcamentoController::class, 'create'])->name('orcamento.create');
